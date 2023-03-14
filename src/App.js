1ba1de64fcfc9b1a941c24d2ab6ee6e40a9a555c
sinan <sinan@sinans-MacBook-Air.local>
2023-03-14 12:21:41 +0530

    router update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import { Routes,Route} from "react-router-dom"
 /**
  * ?  =====Import Components=====
  */
 import Home from './Pages/Home';
 
-function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+function App(){
+  console.log("sinan")
+  return(
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+  )
 }
 
 export default App;
